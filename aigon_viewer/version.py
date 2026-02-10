d1541734703d41856f27f390987512061d177a09
Stefan K Loesch <skloesch_git@topaze.blue>
2026-02-10 21:44:44 +0100

    v0.9

---
+++ aigon_viewer/version.py
@@ -0,0 +1,5 @@
+"""Version information for the aigon_viewer module."""
+
+# VERSIONHERE:AIGON_VIEWER
+__version__ = "0.9"
+__date__ = "2026-02-10"
--- version.py
+++
@@ -1,5 +0,0 @@
-"""Version information for the app_aigon_viewer_server module."""
-
-# VERSIONHERE:APP_AIGON_VIEWER_SERVER
-__version__ = "0.7.2"
-__date__ = "2026-01-05"
